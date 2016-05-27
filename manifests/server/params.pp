f49366c5ea9c89df3dda23e892f7be93e8554c4d
Reuben Avery <ravery@bitswarm.io>
2016-05-27 10:54:04 -0400

    bsl_puppet::server::params class

---
+++ manifests/server/params.pp
@@ -0,0 +1,28 @@
+class bsl_puppet::server::params {
+  $server_common_modules_path = ["/etc/puppetlabs/code/environments/common/modules", "/etc/puppetlabs/code/modules", "/opt/puppetlabs/puppet/modules"]
+  $server_core_modules_path  = ["/etc/puppetlabs/code/infrastructure/${::environment}/modules"]
+  $server_jvm_min_heap_size = '512M'
+  $server_jvm_max_heap_size = '900M'
+  $use_foreman = false
+  $external_nodes = false
+
+  $hiera_datadir = '/etc/puppetlabs/code/infrastructure/%{::environment}/hieradata'
+  $hiera_backends = [ 'yaml' ]
+  $hiera_hierarchy = [
+    'infrastructure/%{environment}/hieradata/defaults',
+    'infrastructure/%{environment}/hieradata/nodes/%{::trusted.certname}',
+    'infrastructure/%{environment}/hieradata/bootstrap/%{::app_project}',
+    'environments/%{environment}/hieradata/%{::aws_tag_profile}',
+    'environments/%{environment}/hieradata/%{::aws_tag_role}',
+    'environments/%{environment}/hieradata/%{::aws_tag_environment}',
+    'environments/%{environment}/hieradata/nodes/%{::trusted.certname}',
+    'infrastructure/%{environment}/hieradata/nodes/%{::trusted.certname}',
+    'infrastructure/%{environment}/hieradata/common',
+  ]
+
+  $hiera_config_path = '/etc/puppetlabs/code/hiera.yaml'
+  $hiera_logger = 'puppet'
+  $hiera_merge_behavior = 'deep'
+
+  $confdir = '/etc/puppetlabs/puppet'
+}
