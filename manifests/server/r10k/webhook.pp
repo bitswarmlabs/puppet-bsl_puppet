059aecba4ca1aefecc374a8c700ed050085c19a8
Reuben Avery <ravery@bitswarm.io>
2016-05-29 20:39:02 -0400

    r10k::webhook dev

--- manifests/server/r10k/webhook.pp
+++ manifests/server/r10k/webhook.pp
@@ -2,11 +2,12 @@ class bsl_puppet::server::r10k::webhook {
   include 'bsl_puppet::server::r10k'
 
   class { '::r10k::webhook::config':
-    # enable_ssl      => false,
+    enable_ssl      => true,
     protected       => true,
     use_mcollective => str2bool($bsl_puppet::server::r10k::use_mcollective),
     user            => $bsl_puppet::server::r10k::webhook_user,
     pass            => $bsl_puppet::server::r10k::webhook_pass,
+    notify     => Service['webhook'],
   }
   ->
   class { '::r10k::webhook':
