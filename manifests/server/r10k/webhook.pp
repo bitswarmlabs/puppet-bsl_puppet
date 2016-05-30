91bc863993e1827b5585455dbf108b9de0b003b8
Reuben Avery <ravery@bitswarm.io>
2016-05-30 12:16:19 -0400

    disabling webhook ssl, should revisit

--- manifests/server/r10k/webhook.pp
+++ manifests/server/r10k/webhook.pp
@@ -1,5 +1,5 @@
 class bsl_puppet::server::r10k::webhook(
-  $enable_ssl = 'true',
+  $enable_ssl = 'false',
   $port = 8088,
 ) {
   include 'bsl_puppet::server::r10k'
@@ -12,8 +12,8 @@ class bsl_puppet::server::r10k::webhook(
     enable_ssl       => str2bool($enable_ssl),
     # certname         => '',
     # certpath         => '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem',
-    public_key_path  => '/etc/puppetlabs/puppet/ssl/ca/ca_pub.pem',
-    private_key_path => '/etc/puppetlabs/puppet/ssl/ca/ca_key.pem',
+    # public_key_path  => '/etc/puppetlabs/puppet/ssl/ca/ca_pub.pem',
+    # private_key_path => '/etc/puppetlabs/puppet/ssl/ca/ca_key.pem',
     protected        => true,
     use_mcollective  => str2bool($bsl_puppet::server::r10k::use_mcollective),
     user             => $bsl_puppet::server::r10k::webhook_user,
